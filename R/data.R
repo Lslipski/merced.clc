8ec6a9aeda82c1a979d847ef7a0febd59bf70691
lslipski <lslipski602@gmail.com>
2025-09-04 09:41:43 -0700

    initial commit with basic load functions

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' ca_regions_fips
+#'
+#' Basic California region and county information for grouping mortality geographies
+#'
+#' @format ## `ca_regions_fips`
+#' A tibble with 58 rows and 5 columns:
+#' \describe{
+#'   \item{region}{Basic region number}
+#'   \item{region_name}{Basic region names}
+#'   \item{county_name}{County Name}
+#'   \item{county_fips}{County Federal Information Processing Standard code}
+#'   \item{county_cdph}{County CA Department of Public Health code}
+#' }
+#' @source <https://github.com/mkiang/excess_drug_overdoses/blob/main/code/utils.R>
+"ca_regions_fips"
---
+++ R/load.R
@@ -0,0 +1,82 @@
+#' load_ca_min_wage
+#'
+#' @importFrom dplyr tribble
+#' @returns tibble with a row containing the year and the CA minimum wage for that year.
+#' @export
+load_ca_min_wage <- function() {
+  # source: https://www.dol.gov/agencies/whd/state/minimum-wage/history
+
+  dplyr::tribble(~year, ~min_wage,
+          2014, 9,
+          2015, 9,
+          2016, 10,
+          2017, 10,
+          2018, 11,
+          2019, 12,
+          2020, 12,
+          2021, 13,
+          2022, 14,
+          2023, 15.5,
+          2024, 16)
+}
+
+
+#' load_fwhs_regions
+#'
+#' @importFrom dplyr tribble
+#' @returns tibble containing a row for every county and its corresponding region
+#' contained in the Farmworker Health Study
+#' @export
+load_fwhs_regions <- function() {
+
+  dplyr::tribble(~county_name, ~region_name,
+          # San Joaquin Valley
+          "San Joaquin", "San Joaquin Valley",
+          "Stanislaus", "San Joaquin Valley",
+          "Merced", "San Joaquin Valley",
+          "Madera", "San Joaquin Valley",
+          "Fresno", "San Joaquin Valley",
+          "Kings", "San Joaquin Valley",
+          "Kern", "San Joaquin Valley",
+          "Tulare", "San Joaquin Valley",
+          # Imperial Valley
+          "Imperial", "Imperial Valley",
+          #Napa/Sonoma
+          "Napa", "Napa/Sonoma",
+          "Sonoma", "Napa/Sonoma",
+          #Upper Central Coast
+          "Monterey", "Upper Central Coast",
+          "Contra Costa", "Upper Central Coast",
+          # Lower Central Coast
+          "Santa Barbara", "Lower Central Coast",
+          "Ventura" , "Lower Central Coast")
+
+}
+
+
+#' load_cv_counties
+#'
+#' @importFrom dplyr tribble
+#' @returns character vector of the counties in the central valley
+#' @export
+load_cv_counties <- function() {
+
+  c("Fresno",
+    "Kern",
+    "Kings",
+    "Madera",
+    "Merced",
+    "San Joaquin",
+    "Stanislaus",
+    "Tulare")
+}
+
+
+#' load_ca_regions
+#'
+#' @returns a tibble containing a row for each county in California and a column for that
+#' countys region (code), region_name, county_name, county FIPS, and the county CDPH code.
+#' @export
+load_ca_regions <- function() {
+  load("data/ca_regions_fips.rda")
+}
