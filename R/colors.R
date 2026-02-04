072313287c5f6c02ff52719c5a9a9ccfdccf31d1
lslipski <lslipski602@gmail.com>
2026-02-04 11:18:04 -0800

    updated documentation of colors functions

--- R/colors.R
+++ R/colors.R
@@ -1,5 +1,10 @@
 
 
+#' list_merced_colors
+#'
+#' @returns list of available merced colors
+#' @export
+#'
 list_merced_colors <- function() {
   return(list(
     primary_blue = "#002856",
@@ -8,6 +13,13 @@ list_merced_colors <- function() {
 }
 
 
+#' get_merced_color
+#'
+#' @param color_name
+#'
+#' @returns string of the hex code correpsonding to a given merced color
+#' @export
+#'
 get_merced_color <- function(color_name) {
   merced_colors = list_merced_colors()
   return(merced_colors[[color_name]])
